5ecb4cd0193dbe23bab043d702634381ab5c164b
Sangbum Woo <Sangbum.Woo@partner.bmw.co.kr>
2021-01-31 16:54:54 +0900

    add firebase http servcie

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -21,9 +21,9 @@ export class SecondComponent implements OnInit {
   message = {}
   postData = {
     kakaoServerKey: "795b9b3c9b9883a36c6ba4d1ea86f043",
-    firebaseServerKey: "795b9b3c9b9883a36c6ba4d1ea86f043",
+    firebaseServerKey: "AAAA2d5bi1A:APA91bE85UoDBkrrhtmptRPnd8JAR41eg7JxXCfwX5vsh8WdA7W0EXcocSo-2UrbKz71AZPqfSCDL7VJ-oYP1d1MTAamjEIi-mm51X1xFlywS5W-P2TAfa8G7EuMs53dqwd-6v6v__xV",
     push_token: "eByxX8IdQK6RPSgaNtU_Pu:APA91bFR7uILyJ2CzWi1j-3B3myMsgPcDlxUb2gFYGJigVl51z5G8i_1GP_7MI1sa0qbeoyO1lgn8RpurFs1LCrEeYKxfENTj9uqxIyYqJz9vHkDO53MGLrfgTpG_Q5e2SvPax6_C4_p",
-    messageTitle : "Message title",
+    messageTitle: "Message title",
     messageBody: "Message body"
   }
 
@@ -39,11 +39,18 @@ export class SecondComponent implements OnInit {
     })
   }
 
-  sendMessageToFirebase() {
+  sendMessageToFirebaseAdmin() {
     console.log("clicked");
     this.pushMessageService.sendMessageToFirebase(this.postData).subscribe(res => {
       console.log(res);
     })
   }
 
+  sendMessageToFirebaseHttp() {
+    console.log("clicked");
+
+    this.pushMessageService.sendMessageToFirebaseHttp(this.postData).subscribe(res => {
+      console.log(res);
+    })
+  }
 }
--- src/app/services/push-message.service.ts
+++ src/app/services/push-message.service.ts
@@ -33,8 +33,18 @@ export class PushMessageService {
       .pipe(
         catchError(this.errorHandler)
       )
-
   }
+
+  sendMessageToFirebaseHttp(message: any) {
+    console.log("sendMessageToFirebase", message);
+    return this.httpClient.post(this.apiServer + '/firebase/send-message-http', message, this.httpOptions)
+      .pipe(
+        catchError(this.errorHandler)
+      )
+  }
+
+  // Firebase Server Key
+  // AAAA2d5bi1A:APA91bE85UoDBkrrhtmptRPnd8JAR41eg7JxXCfwX5vsh8WdA7W0EXcocSo-2UrbKz71AZPqfSCDL7VJ-oYP1d1MTAamjEIi-mm51X1xFlywS5W-P2TAfa8G7EuMs53dqwd-6v6v__xV
 
 
   errorHandler(error: { error: { message: string; }; status: any; message: any; }) {
